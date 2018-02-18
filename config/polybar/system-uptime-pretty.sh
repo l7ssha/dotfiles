345ea7a932e20617f86daa314870d6e9b354688e
l7ssha <szymon.uglis@hotmail.com>
2018-02-18 17:10:56 +0100

    Vscode setting adjust

--- config/polybar/system-uptime-pretty.sh
+++ config/polybar/system-uptime-pretty.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/'
+echo "$(uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/')"
