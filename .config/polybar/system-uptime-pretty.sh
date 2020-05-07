dbd14c9b6a38daceff8cd2e4378ab7a2dc23b247
Szymon Uglis <szymon.uglis@tuta.io>
2020-05-07 22:07:44 +0200

    Clean up

--- .config/polybar/system-uptime-pretty.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "$(uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/')"
--- .config/screenshot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-date=`date +%s`
-
-maim -s ~/.screenshots/$date.png
-xclip -selection clipboard -t image/png < ~/.screenshots/$date.png
